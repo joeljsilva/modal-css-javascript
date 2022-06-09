eff3eed7640e4f90f179b93dbfb28b82be9faaf8
joeljsilva <joel.pjds@gmail.com>
2022-06-09 13:13:28 -0300

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# modal-css-javascript
+# Modal  📜
+
+Modal simples de Login, apesar de ser um projeto bem simples, Ele é muito útil para treinar o seu CSS, animações com keyframes e manipulação do Dom com Javascript.
+
+<img src="./imgs/modalgif.gif">
+
+Você também pode visualizar ele pelo Pages do Github, por aqui:
+https://joeljsilva.github.io/modal-css-javascript/
+
+<br>
+
+[<img alt="alt_text" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" />](https://www.linkedin.com/in/joelpjs/)
+
---
+++ js/script.js
@@ -0,0 +1,21 @@
+function initModal(){
+    const abrirModal = document.querySelector('[data-modal="abrir"');
+    const fecharModal = document.querySelector('[data-modal="fechar"]');
+    const modalContainer = document.querySelector('[data-modal="container"]');
+
+    function showModal(event) {
+      event.preventDefault();
+      modalContainer.classList.toggle('ativo');
+    };
+
+    function sairModalFora(event) {
+      if (event.target === this) {
+        showModal();
+      }
+    };
+
+    abrirModal.addEventListener('click', showModal);
+    fecharModal.addEventListener('click', showModal);
+    modalContainer.addEventListener('click', sairModalFora);
+}
+initModal();
